7e1aa61b99f011a9d55dac1413abf40812439824
akers2534 <akers2534@gmail.com>
2023-01-05 01:16:40 +0000

    finished domain name Gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "it's", "our"];
+  let adj = ["silly", "fun", "fast"];
+  let noun = ["cake", "shoes", "house"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let div = document.createElement("div");
+        div.innerText = `${pronoun[i]}${adj[j]}${noun[k]}.com `;
+        document.body.appendChild(div);
+      }
+    }
+  }
 };
